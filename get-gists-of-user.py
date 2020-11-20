f64bd1a0b3f603442ca0f8a71c2223a6c42ae4ae
Kathirvel Rajendran <kathirvel.rajendran@vvdntech.in>
2020-11-20 15:37:48 +0530

    HTML file generation activated.

--- get-gists-of-user.py
+++ get-gists-of-user.py
@@ -1,9 +1,11 @@
+from jinja2 import Environment, FileSystemLoader
+
 import requests
 
 # Username of Gists
 username = 'linuxkathirvel'
 # Gists API URL
-gists_url = 'https://api.github.com/users/linuxkathirvel/gists'
+gists_url = 'https://api.github.com/users/{}/gists'.format(username)
 # Results per page (max 100)
 per_page = 100
 # Page number of the results to fetch.
@@ -27,15 +29,34 @@ for page_number in range(1, page):
                 gists_list.append(
                     {
                         'html_url': gist.get('html_url'),
-                        'description': gist.get('description') if gist.get('description') else gist['files']
+                        'description': gist.get('description') if gist.get('description') else gist['html_url']
                     }
                 )
         else:
             break
     else:
         print("Status code:", r.status_code)
+context = {
+    'username': username,
+    'gists_url_of_user': 'https://gist.github.com/{}'.format(username),
+}
+
+file_loader = FileSystemLoader('.')
+env = Environment(loader=file_loader)
+
+template = env.get_template('gists.html')
+
 if gists_list:
     for count, gist in enumerate(gists_list):
         print(count, gist.get('html_url'), gist.get('description'))
 else:
     print("No Gists.")
+context['gists'] = gists_list
+
+output = template.render(data=context)
+print(output)
+print(type(output))
+# Write the template in separate HTML
+with open('gists-output.html', 'w') as f:
+    f.write(output)
+
